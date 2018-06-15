b269d2dcb75881d550d0fa12274c41425199f9d0
Leandro Simões <leandro.simoes@outlook.com>
2018-06-15 00:00:12 -0300

    fix exercise url

--- javascript/basics/check-if-is-palindrome.js
+++ javascript/basics/check-if-is-palindrome.js
@@ -1,5 +1,5 @@
 /*
-    Exercice from https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+    Exercice from https://www.w3resource.com/javascript-exercises/javascript-functions-exercises.php
 
     Write a JavaScript function that checks whether a passed string is palindrome or not
         Note: A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run
